cad1e57f8a2573aace520859cacb892717138a15
1031646772 <110569728+1031646772@users.noreply.github.com>
2024-02-27 10:35:56 -0500

    Update app.js

--- BackEnd/src/config/app.js
+++ BackEnd/src/config/app.js
@@ -1,17 +1,27 @@
-const express=require('express');
-const cors=require('cors');
-const app=express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+// Middleware para parsear JSON y datos de formulario
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(cors());
-//Para exportar 
-module.exports=app
-//enpoints, que son como las rutas que identifican cada tarea
-const productosRoute=require('../routes/productos.routes');
-const UserRoutes=require("../routes/Users.routes");
-//creamos un enpoint para identificar las funciones de solo el usuario empleando la constante app
-app.use('/producto',productosRoute); 
-app.use('/users',UserRoutes);
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware de CORS
+app.use(cors({
+  origin: 'https://api-galletasyhojaldres.com', // Cambiar al dominio específico del frontend
+  methods: ['GET', 'POST'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+  credentials: true // Permitir credenciales en las solicitudes
+}));
+
+// Rutas
+const productosRoute = require('../routes/productos.routes');
+const userRoutes = require('../routes/Users.routes');
+
+app.use('/producto', productosRoute);
+app.use('/users', userRoutes);
+
+// Exportar la aplicación
+module.exports = app;
 
 

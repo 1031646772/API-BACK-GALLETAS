ef3d1e94eb7f1f6bafc808b154667c7fcf90149a
HighPear <125331289+OscarCode10@users.noreply.github.com>
2024-04-19 17:31:07 -0500

    cambios, parece que ya sirve

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ const mysql2 = require('mysql2');
 const { createConnection } = require('mysql2');
 
 const database = mysql2.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST || 'http://roundhouse.proxy.rlwy.net/',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || 'HHg2fbH156365a5Dg2GEbBheceH6Dfaf',
+    database: process.env.MYSQL_DATABASE || 'railway',
+    port: process.env.MYSQLPORT || 10784,
 });
 
 module.exports = database;

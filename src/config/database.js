49bf8d97063ba6bc9168e93e572f01c37ba15280
HighPear <125331289+OscarCode10@users.noreply.github.com>
2024-04-19 18:02:52 -0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ const mysql2 = require('mysql2');
 const { createConnection } = require('mysql2');
 
 const database = mysql2.createConnection({
-    host: process.env.MYSQLHOST || 'http://roundhouse.proxy.rlwy.net/',
+    host: process.env.MYSQLHOST || 'roundhouse.proxy.rlwy.net',
     user: process.env.MYSQLUSER || 'root',
-    password: process.env.MYSQLPASSWORD || 'HHg2fbH156365a5Dg2GEbBheceH6Dfaf',
+    password: process.env.MYSQLPASSWORD || 'tNFwBsbdBYuMzLNnitKirkfNEjjYDZjf',
     database: process.env.MYSQL_DATABASE || 'railway',
-    port: process.env.MYSQLPORT || 10784,
+    port: process.env.MYSQLPORT || 54946,
 });
 
 module.exports = database;

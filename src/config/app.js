4ba009ed35d1a440840c1f5ffd4835b0edfd34c7
HighPear <125331289+OscarCode10@users.noreply.github.com>
2024-04-19 20:27:10 -0500

    Update app.js

--- src/config/app.js
+++ src/config/app.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
 // Middleware para parsear JSON y datos de formulario
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware de CORS
-app.use(cors({
-  origin: 'https://front-galletas.vercel.app', // Cambiar al dominio específico del frontend
-  methods: ['GET', 'POST'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-  credentials: true // Permitir credenciales en las solicitudes
-}));
 
 // Rutas
 const productosRoute = require('./../routes/productos.routes');

341667005c1991c68d70586a1d3b3529def51d25
1031646772 <110569728+1031646772@users.noreply.github.com>
2024-04-22 16:25:49 -0500

    Cors

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(cors());
+
 // Middleware para parsear JSON y datos de formulario
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

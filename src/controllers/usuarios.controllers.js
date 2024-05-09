4cdc464e1575a3a0ea67ad44c3d308a85242d1e6
1031646772 <110569728+1031646772@users.noreply.github.com>
2024-05-09 13:06:21 -0500

    Update usuarios.controllers.js
    Se añadio nueva version del token

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -2,17 +2,6 @@ const { query } = require('express');
 const database=require('../config/database')
 const mysql2=require ('mysql2');
 
-let objeto={
-    Nombre:"",
-    Email:"",
-    Contrasena:"",
-    Token:0
-}
-
-function getObjeto(){
-    return objeto
-}
-
 function Ingresar(req, res){
     const {User, Contrasena}=req.body
     try{
@@ -65,24 +54,68 @@ function GenerarTokenIniciosesion(req, res){
 
 function crearToken(datosUsuario){
     let token = Math.floor(Math.random() * 900000) + 100000;
+    const fechaActual = new Date();
 
-    objeto.Nombre=datosUsuario.Nombre,
-    objeto.Email=datosUsuario.Email,
-    objeto.Contrasena=datosUsuario.Contrasena,
-    objeto.Token=token
-    getObjeto()
+    // Obtiene los componentes de la fecha y la hora
+    const year = fechaActual.getFullYear();
+    const month = String(fechaActual.getMonth() + 1).padStart(2, '0');
+    const day = String(fechaActual.getDate()).padStart(2, '0');
+    const hours = String(fechaActual.getHours()).padStart(2, '0');
+    const minutes = String(fechaActual.getMinutes()).padStart(2, '0');
+    const seconds = String(fechaActual.getSeconds()).padStart(2, '0');
+    
+    // Construye la fecha y hora en formato "YYYY-MM-DD HH:MM:SS"
+    const fechaHoraFormateada = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    let objeto={
+        Id:datosUsuario.Id_user,
+        Nombre:datosUsuario.Nombre,
+        Email:datosUsuario.Email,
+        Contrasena:datosUsuario.Contrasena,
+        Token:token,
+        Fecha_Conexion:fechaHoraFormateada
+    }
+    try{
+        const Squery=`
+        insert into TokenUser (Nombre, Email, Contrasena,Token, Fecha_Conexion, Fecha_Desconexion,UserId) values 
+        (${objeto.Nombre},${objeto.Email},${objeto.Contrasena},${objeto.Token},${objeto.Fecha_Conexion},'',${objeto.Id});`
+         const Exquery= mysql2.format(Squery)
+         database.query(Exquery, (err, result)=>{
+           if (err) throw err
+           else{
+             res.json({message:"Se obtuvo el token correctamente"})
+           }
+         })
+   }
+   catch(Err){
+        console.log("No se pudo ejecutar query", Err)
+   }
 }
 
 function TraerToken(req, res){
     const {confirmacion}=req.body
     try{
         console.log(confirmacion)
-        console.log(objeto.Token)
-        if (confirmacion==true && objeto.Token!=0){
-            res.json({message:objeto})
+        const fechaActual=new Date()
+        const dia=String(fechaActual.getDate()).padStart(2, '0');
+        const año=fechaActual.getFullYear();
+        if (confirmacion==true){
+            const Squery=`select * from TokenUser where Email='Juliperalta1306@gmail.com' and Contrasena='JulianPeralta123' and UserId=1 and year (Fecha_Conexion)=${año} and day(Fecha_Conexion)=${dia};`;
+            const Exquery=mysql2.format(Squery)
+            database.query(Exquery, (err, result)=>{
+                if (err) throw err
+                else{
+                    if (result[0] !==undefined){
+                        res.json({message:result[0]});
+                    }
+                    else{
+                        res.json({message:"Usuario no encontrado"})
+                    }
+                }
+            })
         }
         else{
-            res.json({message:"El token o la confirmacion estan fallando"})
+            res.json({message:"La confirmacion esta fallando"})
         }
     }
     catch(err){
@@ -94,12 +127,27 @@ function cerrarSesion(req,res){
     const {confirm}=req.body;
     try{
         if (confirm==true){
-            objeto.Nombre="",
-            objeto.Email="",
-            objeto.Contrasena="",
-            objeto.Token=0,
-            getObjeto()
-            res.json({message:"Se cerro Sesion"});
+            const fechaActual = new Date();
+
+            // Obtiene los componentes de la fecha y la hora
+            const year = fechaActual.getFullYear();
+            const month = String(fechaActual.getMonth() + 1).padStart(2, '0');
+            const day = String(fechaActual.getDate()).padStart(2, '0');
+            const hours = String(fechaActual.getHours()).padStart(2, '0');
+            const minutes = String(fechaActual.getMinutes()).padStart(2, '0');
+            const seconds = String(fechaActual.getSeconds()).padStart(2, '0');
+            
+            // Construye la fecha y hora en formato "YYYY-MM-DD HH:MM:SS"
+            const fechaHoraFormateada = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+            const Squery=`
+            update TokenUser set Fecha_Desconexion=${fechaHoraFormateada} where UserId=1  order by Fecha_Conexion Desc limit 1;`
+             const Exquery= mysql2.format(Squery)
+             database.query(Exquery, (err, result)=>{
+            if (err) throw err
+               else{
+                 res.json({message:"Se borro el token"})
+               }
+             })
         }
         else{
             res.json({message:"Algo fallo al limpiar el usuario"})
@@ -117,4 +165,6 @@ module.exports={
     TraerToken,
     cerrarSesion
 }
+
+
 

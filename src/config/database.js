d6aa02c39ba49b705948b1953fdf3b94799eb8e6
1031646772 <110569728+1031646772@users.noreply.github.com>
2024-05-09 09:22:03 -0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mysql2 = require('mysql2');
 const { createConnection } = require('mysql2');
 
 const database = mysql2.createConnection({
-    host: process.env.MYSQLHOST || 'http://galletasyhojaldres.mysql.database.azure.com/',
+    host: process.env.MYSQLHOST || 'galletasyhojaldres.mysql.database.azure.com',
     user: process.env.MYSQLUSER || 'root1',
     password: process.env.MYSQLPASSWORD || 'juli1234!',
     database: process.env.MYSQL_DATABASE || 'galletasyhojaldres',
